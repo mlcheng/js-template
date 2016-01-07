3aafd404995846fbff609602af7f4c9356b7f5fa
Michael Cheng <michael@michaelcheng.us>
2016-01-07 18:50:19 +0800

    Defer parser

--- template.js
+++ template.js
@@ -15,33 +15,35 @@ var iqwerty = iqwerty || {};
 
 iqwerty.template = (function() {
 	function GetTemplates() {
-		if(typeof $http === 'undefined') {
-			console.error('$http is not defined. Remember to include the iQwerty $http library in your code. Get it here https://github.com/mlcheng/js-http');
-			return;
-		}
+		setTimeout(function() {
+			if(typeof $http === 'undefined') {
+				console.error('$http is not defined. Remember to include the iQwerty $http library in your code. Get it here https://github.com/mlcheng/js-http');
+				return;
+			}
 
-		var templates = document.querySelectorAll('[data-iq-template-src]');
-		[].slice.call(templates).forEach(template => {
-			var src = template.dataset.iqTemplateSrc;
-			var callback = template.dataset.iqTemplateCallback;
-			callback = window[callback];
-			
-			$http(src)
-				.success(response => {
-					template.insertAdjacentHTML('afterbegin', response);
-					if(typeof callback === 'function') {
-						callback(response);
-					} else {
-						if(callback != null) {
-							console.error('Your callback is not defined in your code');
+			var templates = document.querySelectorAll('[data-iq-template-src]');
+			[].slice.call(templates).forEach(template => {
+				var src = template.dataset.iqTemplateSrc;
+				var callback = template.dataset.iqTemplateCallback;
+				callback = window[callback];
+				
+				$http(src)
+					.success(response => {
+						template.insertAdjacentHTML('afterbegin', response);
+						if(typeof callback === 'function') {
+							callback(response);
+						} else {
+							if(callback != null) {
+								console.error('Your callback is not defined in your code');
+							}
 						}
-					}
-				})
-				.error(() => {
-					console.warn('Could not retrieve template');
-				})
-				.get();
-		});
+					})
+					.error(() => {
+						console.warn('Could not retrieve template');
+					})
+					.get();
+			});
+		}, 0);
 	}
 
 	return {
